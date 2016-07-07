a1955b3cab28567f283e6892e3514d6b9bc78a18
itc_user <milacukierman@gmail.com>
2016-07-07 13:26:28 +0300

    coding...

--- JS/js_minecraft.js
+++ JS/js_minecraft.js
@@ -5,79 +5,71 @@
 
 var minecraft = {};
 
+minecraft.tileCodes = {"0":"sky","1":"earth" , "2":"leaf", "3":"tree", "4":"rock"};
+
 minecraft.init = function (){
-    minecraft.creatBoard();
-    minecraft.tool = $('#axe');
-    minecraft.clickedBox = "";
-    minecraft.currentBox = "";
-
-    minecraft.tools =$('.tools');
-    minecraft.tools.on('click',minecraft.getClickedTool);
-
-    minecraft.board =$('.box');
-    minecraft.board.on('click',minecraft.getClickedBoxes);
-    minecraft.checkIfValidSelection()
-
+    $(document).ready(function(){
+        minecraft.creatBoard();
+        $('.tool').click(function(){
+            $(".tool.selected").removeClass("selected");
+            $(this).addClass("selected");
+        });
+    });
 };
 
 minecraft.creatBoard = function () {
 
     minecraft.matrix= [
-        ["sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky"],
-        ["sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky"],
-        ["sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky"],
-        ["sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky"],
-        ["sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky"],
-        ["sky","sky","sky","sky","sky","sky","leaf","leaf","leaf","sky","sky","sky","sky","sky","sky","sky"],
-        ["sky","sky","sky","sky","sky","sky","leaf","leaf","leaf","sky","sky","sky","sky","sky","sky","sky"],
-        ["sky","sky","sky","sky","sky","sky","leaf","leaf","leaf","sky","sky","sky","sky","sky","sky","sky"],
-        ["sky","sky","sky","sky","sky","sky","sky","tree","sky","sky","sky","sky","sky","sky","sky","sky"],
-        ["sky","sky","sky","sky","sky","sky","sky","tree","sky","sky","sky","sky","sky","sky","sky","sky"],
-        ["sky","sky","sky","sky","sky","sky","sky","tree","sky","sky","sky","sky","sky","sky","sky","sky"],
-        ["sky","sky","rock","sky","sky","sky","sky","tree","sky","sky","sky","sky","sky","sky","sky","sky"],
-        ["sky","sky","rock","rock","sky","sky","rock","tree","rock","sky","sky","sky","sky","sky","sky","sky"],
-        ["earth","earth","earth","earth","earth","earth","earth","earth","earth","earth","earth","earth","earth","earth","earth","earth"],
-        ["earth","earth","earth","earth","earth","earth","earth","earth","earth","earth","earth","earth","earth","earth","earth","earth"],
-        ["earth","earth","earth","earth","earth","earth","earth","earth","earth","earth","earth","earth","earth","earth","earth","earth"],
-        ["earth","earth","earth","earth","earth","earth","earth","earth","earth","earth","earth","earth","earth","earth","earth","earth"],
-        ["earth","earth","earth","earth","earth","earth","earth","earth","earth","earth","earth","earth","earth","earth","earth","earth"]
+        ["0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0"],
+        ["0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0"],
+        ["0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0"],
+        ["0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0"],
+        ["0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0"],
+        ["0","0","0","0","0","0","2","2","2","0","0","0","0","0","0","0"],
+        ["0","0","0","0","0","0","2","2","2","0","0","0","0","0","0","0"],
+        ["0","0","0","0","0","0","2","2","2","0","0","0","0","0","0","0"],
+        ["0","0","0","0","0","0","0","3","0","0","0","0","0","0","0","0"],
+        ["0","0","0","0","0","0","0","3","0","0","0","0","0","0","0","0"],
+        ["0","0","0","0","0","0","0","3","0","0","0","0","0","0","0","0"],
+        ["0","0","4","0","0","0","0","3","0","0","0","0","0","0","0","0"],
+        ["0","0","4","4","0","0","4","3","4","0","0","0","0","0","0","0"],
+        ["1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1"],
+        ["1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1"],
+        ["1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1"],
+        ["1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1"],
+        ["1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1"]
     ];
     for (var i = 0; i < minecraft.matrix.length; i++) {
         for (var j = 0; j < minecraft.matrix[i].length; j++) {
-            $('<div/>').addClass(minecraft.matrix[i][j]).addClass('box').appendTo($('.board'));
+            var tileName = minecraft.tileCodes[minecraft.matrix[i][j]];
+            $('<div/>').addClass(tileName).addClass('box').appendTo($('.board')).click(minecraft.checkIfValidSelection);
         }
         $('.board').append($("<br/>"));
     }
 };
 
-minecraft.getClickedTool = function (event){
-    minecraft.tool.on('onclick',.addClass(".blue"));
-    minecraft.tool = event.target.id;
-};
 
-minecraft.getClickedBoxes = function (event){
-    minecraft.clickedBox = event.target.className;
-};
+
+
 
 minecraft.checkIfValidSelection = function (){
     // check if proper tools are being used to select the proper boxes
-    // id axe can select class tree
+    // id axe can select class 3
+    var clickedTile = $(this);
+    if ($('.tool.selected').attr("id")== "axe" && clickedTile.hasClass("tree")){
+    clickedTile.removeClass("tree").addClass("sky");
 
-    if ((minecraft.tool === $('#axe')) && (minecraft.clickedBox === $('.tree'))){
-        minecraft.clickedBox =  minecraft.currentBox;
     }
-    else {
-        minecraft.tool.on('onclick',.addClass(".red"));
+    // id pickaxe can select class rock
+    if ($('.tool.selected').attr("id") == "pickaxe" && clickedTile.hasClass("rock")){
+        clickedTile.removeClass("rock").addClass("sky");
+    }
+    // id shovel can select class 1
+    if ($('.tool.selected').attr("id")=="shovel" && clickedTile.hasClass("earth")){
+        clickedTile.removeClass("earth").addClass("sky");
     }
 
-    // id pickaxe can select class rocks
-    //id shovel can select class earth
-
-}
-
+};
 
 minecraft.init();
 
-//
-// $('.current-player-color').toggleClass("blue-player");
-// $(this).addClass()
